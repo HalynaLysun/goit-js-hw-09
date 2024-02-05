d73502fce7009a5efa235f916b886fa5e67426d0
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-02-05 22:26:22 +0200

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,4 +1,6 @@
+import SimpleLightbox from "simplelightbox";
 
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const images = [
   {
@@ -86,7 +88,7 @@ const items = images
 galleryEl.insertAdjacentHTML('beforeend', items)
 
 
-// const lightbox = new SimpleLightbox('.gallery a', {
-//     captionsData: 'alt',
-//     captionDelay: 250,
-// });
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
